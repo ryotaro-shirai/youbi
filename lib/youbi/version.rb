ab0b3cc77fec14253756f33ada7a610551336681
ryotaro-shirai <ryotaro.shirai@giftee.co>
2025-02-09 12:06:47 +0900

    Generate gem template using bundle gem

---
+++ lib/youbi/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Youbi
+  VERSION = "0.1.0"
+end
---
+++ spec/youbi_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Youbi do
+  it "has a version number" do
+    expect(Youbi::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
